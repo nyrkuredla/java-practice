62549e0e70793aca5b5da5c0d65ca4c3a9a9d8d9
Ryan Kelley <rtkelley@gmail.com>
2018-01-08 07:13:57 -0500

    more beginning

--- helloWorld/src/Hello.java
+++ helloWorld/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello dinosaurs!");
+        int myFirstNumber = 5 + 10;
+        System.out.println(myFirstNumber);
+        System.out.println("5" + 10);
+        System.out.println(5 + "10");
     }
 }
