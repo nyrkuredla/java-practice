a7642870a28d23a4f6c35b74477ad5902e5c4802
Ryan Kelley <rtkelley@gmail.com>
2018-01-09 07:09:15 -0500

    chars and bools

---
+++ charAndBool/src/com/rtkelley/Main.java
@@ -0,0 +1,15 @@
+package com.rtkelley;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// char and bool
+
+        char myChar = 'R'; // one letter or one character, including Unicode
+        char myCyrillicChar = '\u0414'; // see?
+        System.out.println("Cyrillic!: " + myCyrillicChar);
+
+        boolean myBool = true; // true or false
+
+    }
+}
